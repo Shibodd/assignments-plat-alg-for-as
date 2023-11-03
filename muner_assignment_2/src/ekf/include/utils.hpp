87268ed6faf592f42b6d659d7d93330bb3d51e17
Shibodd <simonebondi456@gmail.com>
2023-11-03 14:33:07 +0100

    Add include guard to utils hpp

--- muner_assignment_2/src/ekf/include/utils.hpp
+++ muner_assignment_2/src/ekf/include/utils.hpp
@@ -1,12 +1,19 @@
-namespace utils {
-  constexpr double PI = 3.14159265358979323846;
-  constexpr double TWO_PI = 2 * PI;
+#ifndef UTILS_HPP
+#define UTILS_HPP
 
-  /**
-   * Returns the minimum rotation required for moving from angle x to angle y
-  */
-  inline double angle_difference_radians(double x, double y) {
-    double diff = fmod(y - x + PI, TWO_PI) - PI;
-    return diff < -PI ? diff + TWO_PI : diff; 
-  }
+namespace utils {
+
+constexpr double PI = 3.14159265358979323846;
+constexpr double TWO_PI = 2 * PI;
+
+/**
+ * Returns the minimum rotation required for moving from angle x to angle y
+*/
+inline double angle_difference_radians(double x, double y) {
+  double diff = fmod(y - x + PI, TWO_PI) - PI;
+  return diff < -PI ? diff + TWO_PI : diff; 
 }
+
+} // namespace utils
+
+#endif // !UTILS_HPP
