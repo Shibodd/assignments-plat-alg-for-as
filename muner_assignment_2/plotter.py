915f0b25e0c9cfbcc7a4f996bc6adea181861576
Shibodd <simonebondi456@gmail.com>
2023-11-03 21:05:07 +0100

    Plot

--- muner_assignment_2/plotter.py
+++ muner_assignment_2/plotter.py
@@ -2,23 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-
 # x_est; y_est; x_gt; y_gt; squared_error
 with open('res.txt','r') as file:
   rmse = float(next(file))
   data = pd.read_csv(file, delimiter=';')
 
-print(rmse)
-print(data)
+n = data.shape[0]
 
-exit()
+fig, (ax1, ax2) = plt.subplots(2)
+fig.text(0.2, 0.9, f"RMSE: {rmse}")
+ax1.plot(data['x_est'], data['y_est'])
+ax1.scatter(data['x_gt'], data['y_gt'],color='green', s=5)
 
-fig, (ax1, ax2, ax3) = plt.subplots(3)
-ax1.plot(x, y)
-ax1.scatter(xgt, ygt,color='green', s=5)
-
-t=np.arange(x)
-ax2.plot(t, xrmse)
-ax3.plot(t, yrmse)
+t = np.arange(n)
+ax2.plot(t, data['squared_error'])
 plt.show()
