002bc9b7d557ee6ba17ee048e290f8b0889b56f0
Shibodd <simonebondi456@gmail.com>
2023-12-14 09:06:44 +0100

    Dynamic model

--- modeling/bicycle_dyn_bodyalign_factory.m
+++ modeling/bicycle_dyn_bodyalign_factory.m
@@ -1,17 +1,27 @@
 function xdot_fn = bicycle_dyn_bodyalign_factory(m, I, lf, lr, cf, cr)
     wheelbase = lf + lr;
 
-    function xdot_fn = bicycle_dyn_glob(t, state, inputs)
+    function xdot = bicycle_dyn_glob(t, state, inputs)
         steering = inputs(1);
         v = inputs(2);
 
         momentum = m * v;
+        iv = I * v;
+        twocf = 2*cf;
+        twocr = 2*cr;
         
-        
-        A = [0, 1, 0, 0;
-            ]
-        
-        xdot_fn = [dx; dy; dheading];
+        A = [-(twocf + twocr) / momentum, 0, -v - (twocf * lf - twocr * lr) / momentum;
+             0, 0, 1;
+             -(twocf * lf - twocr * lr) / iv, 0, -(twocf * lf * lf + twocr * lr * lr) / iv];
+
+        B = [twocf / m; 0; twocf * lf / I];
+
+        vy = state(3);
+        phi = state(4);
+        xdot = [v * cos(phi) - vy * sin(phi); % X in global frame
+                v * sin(phi) + vy * cos(phi); % Y in global frame
+                A * state(3:end) + B * steering % vy, phi, phidot
+               ];
     end
     xdot_fn = @bicycle_dyn_glob;
 end
